81575858b9f162a132167c2af1430ca3aac3b248
mvd <mvd@acadon.de>
2025-09-16 18:42:41 +0200

    lollolo

---
+++ buildWin/external/raylib/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/mvd/Schule/repos/raylibTests/external/raylib/src
+# Build directory: C:/Users/mvd/Schule/repos/raylibTests/build/external/raylib/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("external/glfw")

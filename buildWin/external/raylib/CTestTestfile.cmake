4918298fb35ecf68c491a5e2dc744ebcc232f5c0
vando <vando.02800@gmail.com>
2025-09-16 17:03:31 +0200

    llololo

--- buildWin/external/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/mvd/Schule/repos/raylibTests/external/raylib
-# Build directory: C:/Users/mvd/Schule/repos/raylibTests/build/external/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
